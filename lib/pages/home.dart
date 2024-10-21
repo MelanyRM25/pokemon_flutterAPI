38265557e929acdead2262422bfa7657fe34a00d
unknown <melanyrm2507@gmail.com>
2024-10-21 01:17:23 -0400

    Agregando endpoints imagenes pokemon

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,5 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
@@ -11,41 +11,60 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<String> items = ['item 1', 'item2', 'item 3', 'item 4'];
-  // TextEditingController emailController = TextEditingController();
-  // TextEditingController passwordController = TextEditingController();
+  List pokemons = [];
+  @override
+  void initState() {
+    pokemon150Get();
+    super.initState();
+  }
 
-  // void login() async {
-  //   var url = Uri.https('localhost:8000/api/login');
-  //   var response = await http.get(url);
-
-  //   if (response.statusCode == 200) {
-  //     var jsonResponse =
-  //         convert.jsonDecode(response.body) as Map<String, dynamic>;
-  //     var itemCount = jsonResponse['totalItems'];
-  //     print('Number of books about http: $itemCount.');
-  //   } else {
-  //     print('Request failed with status: ${response.statusCode}.');
-  //   }
-  // }
+  pokemon150Get() async {
+    var url = Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=151');
+    var response = await http.get(url);
+    var body = json.decode(response.body);
+    setState(() {
+      pokemons = body['results'];
+    });
+    print(pokemons);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("MI APLICACIÓN"),
-      ),
-      body: Row(
-        children: [
-          Flexible(
-              child: Container(
-            color: Colors.blueGrey,
-          )),
-          Flexible(
-              child: Container(
-            color: Colors.blueAccent,
-          ))
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("MI APLICACIÓN"),
+        ),
+        body: ListView.builder(
+            itemCount: pokemons.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Image.network(
+                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png'),
+                title: Text(pokemons[index]['name']),
+              );
+            }));
   }
 }
+
+//USO DE FLEXIBLE
+ // Row(
+      //   children: [
+      //     Flexible(
+      //         child: Container(
+      //       color: Colors.blueGrey,
+      //     )),
+      //     Flexible(
+      //         child: Container(
+      //       color: Colors.blueAccent,
+      //     ))
+      //   ],
+      // ),
+
+// LISTAS DINAMICAS
+// ListView.builder(
+//                   itemCount: items.length,
+//                   itemBuilder: (context, index) {
+//                     return ListTile(
+//                       title: Text(items[index]),
+//                     );
+//                   }),
