963e0d00654507e4ba1cb28341edc723d324613f
unknown <melanyrm2507@gmail.com>
2024-10-20 23:47:23 -0400

    Creando la interfaz

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,15 +8,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("MI APLICACIÓN"),
-      ),
-      body: Center(
-        child: Text("Home Page"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("MI APLICACIÓN"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: "E-mail"),
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: "Contraseña"),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print(emailController.text);
+                    print(passwordController.text);
+                  },
+                  child: Text("login"))
+            ],
+          ),
+        ));
   }
 }
